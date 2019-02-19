a27a91e28de06a5e2e57380ed1176d454925752b
Samruddhi Somani <ssomani@homeaway.com>
2019-02-18 20:13:54 -0600

    adding cat photos

--- docs/init.js
+++ docs/init.js
@@ -9,7 +9,10 @@ const init = {
 
 // using jQuery and the object above, display the information as the appropriate button is clicked
 
-const tellmemore = '<i class="fab fa-github-square fa-lg"></i>    <i class="fab fa-twitter-square fa-lg"></i>   <i class="fab fa-linkedin fa-lg"></i>'
+const tellmemore =
+    '<a href="https://github.com/samruddhisomani/Showcase"><i class="fab fa-github-square fa-lg"></i></a>   ' +
+    '<a href="https://twitter.com/samruddhisomani"><i class="fab fa-twitter-square fa-lg"></i></a>  ' +
+    '<a href="https://www.linkedin.com/in/samruddhisomani"><i class="fab fa-linkedin fa-lg"></i></a>'
 
 $('#firstname').click(_ => ($('#answerstext').text(init['firstName'])))
 $('#lastname').click(_ => ($('#answerstext').text(init['lastName'])))
@@ -19,3 +22,13 @@ $('#tellmemore').click(_ => {
     $('#answerstext').text("");
     $('#answerstext').append(tellmemore);
 })
+
+$('#catlink').click(_ => {
+    if ($('#catphotoimg')) { $('#catphotoimg').remove() }
+    fetch('https://aws.random.cat/meow')
+        .then(response => response.json())
+        .then(function (myJson) {
+            $('#ex1').prepend(`<img id='catphotoimg' alt='A random cat photo' src=${myJson.file}>`)
+        });
+}
+)
