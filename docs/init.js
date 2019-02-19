6963d5e22fba7f7e7fb1629c7b2719da4e18faad
Samruddhi Somani <ssomani@homeaway.com>
2019-02-18 20:52:43 -0600

    fixing up modal border radius; fixing up font-awesome link colors.

--- docs/init.js
+++ docs/init.js
@@ -1,18 +1,19 @@
 // create a JavaScript object here with the following fields: firstName, lastName, jobTitle, homeOffice
 
-const init = {
-    firstName: 'Samruddhi',
-    lastName: 'Somani',
-    jobTitle: 'Core Bidding Manager',
-    homeOffice: 'Austin DT'
-}
-
-// using jQuery and the object above, display the information as the appropriate button is clicked
-
 const tellmemore =
     '<a href="https://github.com/samruddhisomani/Showcase"><i class="fab fa-github-square fa-lg"></i></a>   ' +
     '<a href="https://twitter.com/samruddhisomani"><i class="fab fa-twitter-square fa-lg"></i></a>  ' +
     '<a href="https://www.linkedin.com/in/samruddhisomani"><i class="fab fa-linkedin fa-lg"></i></a>'
+
+const init = {
+    firstName: 'Samruddhi',
+    lastName: 'Somani',
+    jobTitle: 'Core Bidding Manager',
+    homeOffice: 'Austin DT',
+    tellMeMore: tellmemore
+}
+
+// using jQuery and the object above, display the information as the appropriate button is clicked
 
 $('#firstname').click(_ => ($('#answerstext').text(init['firstName'])))
 $('#lastname').click(_ => ($('#answerstext').text(init['lastName'])))
@@ -20,7 +21,7 @@ $('#jobtitle').click(_ => ($('#answerstext').text(init['jobTitle'])))
 $('#homeoffice').click(_ => ($('#answerstext').text(init['homeOffice'])))
 $('#tellmemore').click(_ => {
     $('#answerstext').text("");
-    $('#answerstext').append(tellmemore);
+    $('#answerstext').append(init['tellMeMore']);
 })
 
 $('#catlink').click(_ => {
